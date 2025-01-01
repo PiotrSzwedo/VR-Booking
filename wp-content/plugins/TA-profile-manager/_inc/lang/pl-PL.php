f8fc94eb1186b0637284dd83e2ce0016e4fcc748
Piotr Szwedo <piotrszwedo2007@gmail.com>
2025-01-01 22:22:52 +0100

    Added register form

--- wp-content/plugins/TA-profile-manager/_inc/lang/pl-PL.php
+++ wp-content/plugins/TA-profile-manager/_inc/lang/pl-PL.php
@@ -5,4 +5,9 @@ return [
     "password" => "hasło",
     "remember_me" => "zapamiętaj mnie",
     "register" => "zarejestruj",
+    "name" => "imię",
+    "forename" => "nazwisko",
+    "create_account" => "utwórz konto",
+    "email" => "E-mail",
+    "username" => "nazwa użytkownika"
 ];
